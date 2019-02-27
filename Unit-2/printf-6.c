08d5854e0aabfffdc173f3ac61ff8e12abb80304
Offliner <eric21904@gmail.com>
2019-02-28 00:24:46 +0800

    Unit-2

---
+++ Unit-2/printf-6.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	printf("We don\'t have enough time\n");
+	printf("And computer is powerfuk\bl\n");
+	printf("Everybody say \"Telchnology always comes from what people want\"\n");
+	printf("Algorithm \151\163 good for solving problem \x6f\x66 life\n");
+	printf("    can make it\rYou\n");
+	/*
+	常用控制字元
+	\n  換行
+	\t  跳格
+	\b  倒退一格 
+	\r  回到此列最前端 
+	\f  跳頁 
+	\'  印出單引號 
+	\"  印出雙引號 
+	\x..  印出ascii十六進位碼所對應字元
+	\o..  印出ascii八進位碼所對應字元
+	ASCII table : https://zh.wikipedia.org/wiki/ASCII 
+	*/ 
+	system("pause");
+	return 0;
+} 
