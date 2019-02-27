253c79edaaa5c2f7a731e8d83587d356132b9c21
Offliner <eric21904@gmail.com>
2019-02-27 21:10:55 +0800

    Unit-2

---
+++ Unit-2/printf-1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	printf("standard functions of I/O(input and output):\n");
+	printf("printf() and scanf()\n");
+	system("pause");
+	return 0;
+}
---
+++ Unit-2/printf-2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	int number = 8;
+	char letter = 'z';
+	printf("1 + 1 = %d\n",2);
+	printf("1 + 7 = %d\n",number);
+	printf("%c is the first English alphabet\n",'a');
+	printf("%c is the last English alphabet\n",letter);
+	// %d 和 %c 是格式特定字 
+	system("pause");
+	return 0;
+}
